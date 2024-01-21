113b280f6ee7dffd59f4531f2e8468690d7aec5f
andykr1k <69021102+andykr1k@users.noreply.github.com>
2024-01-21 00:55:36 -0800

    Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './stacks';
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import { ParticlesBG } from "../components";
+
+export default function HomePage() {
+  return (
+    <>
+      <ParticlesBG />
+      <h1 className="text-red-500">UCR Underground</h1>
+    </>
+  );
+}
